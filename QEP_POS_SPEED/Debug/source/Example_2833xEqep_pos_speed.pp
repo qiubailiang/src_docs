b681535cad39d06f95289ab2d79187adc9da3f68
mongolia19@gmail.com <mongolia19@gmail.com>
2014-07-21 09:55:52 +0800

    Change Map Points
    Change Map Points

--- QEP_POS_SPEED/Debug/source/Example_2833xEqep_pos_speed.pp
+++ QEP_POS_SPEED/Debug/source/Example_2833xEqep_pos_speed.pp
@@ -1,38 +1,38 @@
 # FIXED
 
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/Example_2833xEqep_pos_speed.c
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP28x_Project.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Device.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Adc.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_DevEmu.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_CpuTimers.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_ECan.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_ECap.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_DMA.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_EPwm.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_EQep.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Gpio.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_I2c.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_McBSP.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_PieCtrl.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_PieVect.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Spi.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Sci.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_SysCtrl.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_XIntrupt.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Xintf.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Examples.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_GlobalPrototypes.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_ePwm_defines.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Dma_defines.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_I2C_defines.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_DefaultISR.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/Example_posspeed.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/IQmathLib.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/limits.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/math.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/linkage.h
-C:/Users/MeAndroid/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/float.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/Example_2833xEqep_pos_speed.c
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP28x_Project.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Device.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Adc.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_DevEmu.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_CpuTimers.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_ECan.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_ECap.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_DMA.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_EPwm.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_EQep.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Gpio.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_I2c.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_McBSP.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_PieCtrl.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_PieVect.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Spi.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Sci.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_SysCtrl.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_XIntrupt.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Xintf.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Examples.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_GlobalPrototypes.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_ePwm_defines.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_Dma_defines.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_I2C_defines.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/DSP2833x_DefaultISR.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/Example_posspeed.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: ../source/IQmathLib.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/limits.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/math.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/linkage.h
+F:/Users/Android/Documents/GitHub/Xeuns\ 1116\ CAN-QEP-POS-SPEED/Debug/Example_2833xEqep_pos_speed.obj: D:/Program\ Files/Texas\ Instruments/ccsv4/tools/compiler/c2000/include/float.h
 
 ../source/Example_2833xEqep_pos_speed.c: 
 ../source/DSP28x_Project.h: 
--- QEP_POS_SPEED/source/Example_2833xEqep_pos_speed.c
+++ QEP_POS_SPEED/source/Example_2833xEqep_pos_speed.c
@@ -227,7 +227,7 @@ void main(void)
 	int i=0;
 	for(i=0;i<10;i++)
 	{
-			Map[i].x=300;
+			Map[i].x=0;
 			//Map[i].y=-800+1600/9*i;
 			Map[i].y=-200*i;
 	}
@@ -235,7 +235,7 @@ void main(void)
 	{
 			//Map[i].x=36000-i*3600;
 			//Map[i].y=-800+1600/9*i;
-			Map[i].x=+200;
+			//Map[i].x=+200;
 	}
 	//Map[9].x=2410;
 	//Map[9].y=500;
